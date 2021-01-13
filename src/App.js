51a0656c208e9bb517fda61862f31241f5ed5cbd
camha91 <crystalhanguyen@gmail.com>
2021-01-12 17:48:42 -0800

    Remove an item from todoList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import ColorBox from './component';
+import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, settodoList] = useState([
+    { id: 1, title: 'Learning React Hooks' },
+    { id: 2, title: 'Doing Chat App Project' },
+    { id: 3, title: 'Doing Netflix Project' },
+  ]);
+
+  function handleTodoClick(todo) {
+    const newTodoList = todoList.filter((x) => x.id !== todo.id);
+    return settodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>Welcome to React hooks!</h1>
+      <h1>Todo List</h1>
 
-      <ColorBox />
+      <TodoList
+        todos={todoList}
+        onTodoClick={handleTodoClick}
+      />
     </div>
   );
 }
