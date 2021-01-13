500495cd569d645e6e3c8d24b223fb979c9593d0
camha91 <crystalhanguyen@gmail.com>
2021-01-13 13:54:01 -0800

    Add todoForm

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import ColorBox from './components/ColorBox';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todoList, settodoList] = useState([
@@ -12,13 +13,25 @@ function App() {
 
   function handleTodoClick(todo) {
     const newTodoList = todoList.filter((x) => x.id !== todo.id);
-    return settodoList(newTodoList);
-  }
+    settodoList(newTodoList);
+  };
+
+  function handleTodoFormSubmit(formValues) {
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+    const newTodoList = [...todoList, newTodo];
+    settodoList(newTodoList);
+  };
 
   return (
     <div className="app">
       <h1>Todo List</h1>
 
+      <TodoForm
+        onSubmit={handleTodoFormSubmit}
+      />
       <TodoList
         todos={todoList}
         onTodoClick={handleTodoClick}
