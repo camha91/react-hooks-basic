1fa50fb754833357f616466129781aeea174c00c
camha91 <crystalhanguyen@gmail.com>
2021-01-12 17:08:38 -0800

    Color Box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import ColorBox from './component';
 
 function App() {
   return (
     <div className="app">
       <h1>Welcome to React hooks!</h1>
+
+      <ColorBox />
     </div>
   );
 }
